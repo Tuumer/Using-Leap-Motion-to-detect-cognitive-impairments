4b9e1bd4a1f376667cbbf8e24c1c9fdee8744cc3
SarahAisku <saradzumagalieva@gmail.com>
2024-04-22 23:36:55 +0600

    вфвфв

--- UsingLeapMotion/Assets/Scripts/CDT_Game_Scripts/screenshot_o.cs
+++ UsingLeapMotion/Assets/Scripts/CDT_Game_Scripts/screenshot_o.cs
@@ -1,27 +1,13 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System;
-
+ 
 public class Screenshot : MonoBehaviour
 {
-    public Button screenshotButton; // Reference to the button in the Inspector
-
-    private void Start()
+    private void Update()
     {
-        // Check if the screenshotButton is assigned
-        if (screenshotButton != null)
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            // Add a listener to the button's onClick event
-            screenshotButton.onClick.AddListener(CaptureScreenshot);
+            ScreenCapture.CaptureScreenshot("screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png", 4);
         }
-        else
-        {
-            UnityEngine.Debug.LogError("Screenshot Button is not assigned!");
-        }
-    }
-
-    private void CaptureScreenshot()
-    {
-        ScreenCapture.CaptureScreenshot("screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png", 4);
     }
 }
