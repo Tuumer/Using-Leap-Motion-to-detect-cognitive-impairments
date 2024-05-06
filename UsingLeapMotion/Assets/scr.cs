532ac5c406d64f7e3895aa6819d4d2a2e5726bc1
Yerassyl <fistofsnake@gmail.com>
2024-05-06 18:20:59 +0500

    hgj
    ghj

--- UsingLeapMotion/Assets/scr.cs
+++ UsingLeapMotion/Assets/scr.cs
@@ -5,9 +5,15 @@ public class SpriteCreator : MonoBehaviour
 {
     public GameObject spritePrefab;
     public Texture2D circleTexture;
-    public List<string> initialBellTags = new List<string>() { "bell1", "bell2", "bell3", "bell4", "bell5" };
+    public List<string> initialBellTags = new List<string>() { 
+    "bell1", "bell2", "bell3", "bell4", "bell5", "bell6", "bell7", "bell8", "bell9", "bell10",
+    "bell11", "bell12", "bell13", "bell14", "bell15", "bell16", "bell17", "bell18", "bell19", "bell20",
+    "bell21", "bell22", "bell23", "bell24", "bell25", "bell26", "bell27", "bell28", "bell29", "bell30",
+    "bell31", "bell32", "bell33", "bell34", "bell35"
+    };
 
-    private List<string> clickedBellTags = new List<string>();
+
+    private HashSet<string> clickedBellTags = new HashSet<string>(); // Changed data structure to HashSet
 
     public int count = 0;
 
@@ -15,48 +21,43 @@ public class SpriteCreator : MonoBehaviour
     public LayerMask selectableLayerMask;
 
     void Update()
-{
-    if (Input.GetMouseButtonDown(0)) // Left mouse button
     {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
         {
-            if (hit.collider != null && IsSelectable(hit.collider.gameObject))
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                string tag = hit.collider.gameObject.tag;
-                clickedBellTags.Add(tag);
-
-                
-                
+                if (hit.collider != null && IsSelectable(hit.collider.gameObject))
+                {
+                    string tag = hit.collider.gameObject.tag;
+                    clickedBellTags.Add(tag);
+                }
             }
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10f;
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Instantiate(spritePrefab, worldPosition, Quaternion.identity);
+            DrawCircle(worldPosition, 0.05f);
+            count++;
         }
-        Vector3 mousePosition = Input.mousePosition;
-                mousePosition.z = 10f;
-                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-                Instantiate(spritePrefab, worldPosition, Quaternion.identity);
-                DrawCircle(worldPosition, 0.05f);
-                count++;
-    }
 
-    // Compare the clicked bell tags with the initial list
-    if (Input.GetKeyDown(KeyCode.Space))
-    {
-        CompareTags();
+        // Compare the clicked bell tags with the initial list
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CompareTags();
+        }
     }
-}
-
 
     bool IsSelectable(GameObject obj)
-{
-    // Check if the GameObject is on the selectable layer mask
-    bool selectable = (selectableLayerMask & (1 << obj.layer)) != 0;
-    if (!selectable)
     {
-        Debug.Log(obj.name + " is not selectable because it is not on a selectable layer.");
+        // Check if the GameObject is on the selectable layer mask
+        bool selectable = (selectableLayerMask & (1 << obj.layer)) != 0;
+        if (!selectable)
+        {
+            Debug.Log(obj.name + " is not selectable because it is not on a selectable layer.");
+        }
+        return selectable;
     }
-    return selectable;
-}
-
 
     void DrawCircle(Vector3 position, float radius)
     {
