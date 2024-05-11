e2baf4721c8367110c4669d70870a305fbf6e61b
Yerassyl <fistofsnake@gmail.com>
2024-05-11 23:26:30 +0500

    asd
    asd

--- UsingLeapMotion/Assets/Drawing/Conn_manage.cs
+++ UsingLeapMotion/Assets/Drawing/Conn_manage.cs
@@ -14,6 +14,7 @@ namespace _Project
         private int currentSphereIndex = 0;
         string[] expectedTags = { "tmtstart", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13","14", "15", "16", "17", "18", "19","20", "21", "22", "23", "24", "endtmt" };
         [SerializeField] Text _timerText;
+        public int i;
         private void Start()
         {
             _mainCamera = Camera.main;
