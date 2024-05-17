e96906ca0ed8dd02dfdec7cad9bdebb1156535ed
SarahAisku <saradzumagalieva@gmail.com>
2024-05-17 07:13:37 +0600

    aaaaa

--- UsingLeapMotion/Assets/Scripts/CDT/ScreenShot.cs
+++ UsingLeapMotion/Assets/Scripts/CDT/ScreenShot.cs
@@ -1,18 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.IO;
 
 public class Screenshot : MonoBehaviour
 {
-    public Button screenshotButton; // Reference to the button in the Inspector
+    public Button screenshotButton;
     public GameObject popUp;
+    public GameObject ellipseImage;
+    private string screenshotFolder;
 
     private void Start()
     {
-        // Check if the screenshotButton is assigned
+        // Define the path for the screenshots folder
+        screenshotFolder = Path.Combine(UnityEngine.Application.persistentDataPath, "screenshots");
+
+        // Ensure the folder exists
+        if (!Directory.Exists(screenshotFolder))
+        {
+            Directory.CreateDirectory(screenshotFolder);
+        }
+
+        // Set up the screenshot button listener
         if (screenshotButton != null)
         {
-            // Add a listener to the button's onClick event
             screenshotButton.onClick.AddListener(CaptureScreenshot);
         }
         else
@@ -23,8 +34,26 @@ public class Screenshot : MonoBehaviour
 
     private void CaptureScreenshot()
     {
-        ScreenCapture.CaptureScreenshot("screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png", 4);
+        // Deactivate the ellipse image before taking the screenshot
+        if (ellipseImage != null)
+        {
+            ellipseImage.SetActive(false);
+        }
 
+        // Create the filename for the screenshot
+        string filename = Path.Combine(screenshotFolder, "screenshot-" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".png");
+        UnityEngine.Debug.Log("Saving screenshot to: " + filename);
+
+        // Capture the screenshot
+        ScreenCapture.CaptureScreenshot(filename, 1); // Set scale factor to 1 for normal resolution
+
+        // Reactivate the ellipse image after taking the screenshot
+        if (ellipseImage != null)
+        {
+            Invoke("ActivateEllipseImage", 0.5f); // Adjust the delay as needed
+        }
+
+        // Activate the pop-up if it is assigned
         if (popUp != null)
         {
             Invoke("ActivatePopUp", 0.5f);
@@ -35,9 +64,19 @@ public class Screenshot : MonoBehaviour
         }
     }
 
+    private void ActivateEllipseImage()
+    {
+        ellipseImage.SetActive(true);
+        Invoke("DeactivateEllipseImage", 2.0f); // Deactivate the ellipse image after 2 seconds
+    }
+
+    private void DeactivateEllipseImage()
+    {
+        ellipseImage.SetActive(false);
+    }
+
     private void ActivatePopUp()
     {
-        // Set PopUP GameObject active
         popUp.SetActive(true);
     }
 }
