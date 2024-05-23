c99d0d0cabb41dc1596b693a33cf7d29efe4c110
SarahAisku <saradzumagalieva@gmail.com>
2024-05-23 06:59:00 +0600

    dqdqdqd

--- UsingLeapMotion/Assets/Scripts/CDT/CDT_Result.cs
+++ UsingLeapMotion/Assets/Scripts/CDT/CDT_Result.cs
@@ -13,7 +13,26 @@ public class CDT_Result : MonoBehaviour
     {
         float hour = mouseDraw.GetHourResult();
         float minute = mouseDraw.GetMinuteResult();
-        float sum = hour + minute;
-        UnityEngine.Debug.Log("Sum: " + sum);
+        float hour_score, minute_score;
+
+        if (hour > 3)
+        {
+            hour_score = 1;
+        }
+        else { 
+            hour_score = 0; 
+        }
+
+        if (minute > 3)
+        {
+            minute_score = 1;
+        }
+        else
+        {
+            minute_score = 0;
+        }
+
+        float sum = hour_score + minute_score;
+        UnityEngine.Debug.Log("CDT Result: " + sum);
     }
 }
