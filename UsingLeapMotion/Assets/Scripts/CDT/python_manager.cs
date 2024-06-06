fb2cfee9e5098d4b73563ed77803fd17bbd5a25c
SarahAisku <saradzumagalieva@gmail.com>
2024-06-06 06:26:11 +0600

    fsfsfsf

--- UsingLeapMotion/Assets/Scripts/CDT/python_manager.cs
+++ UsingLeapMotion/Assets/Scripts/CDT/python_manager.cs
@@ -1,6 +1,7 @@
+using UnityEngine;
+using UnityEditor;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor.Scripting.Python;
 
 public class python_manager : MonoBehaviour
